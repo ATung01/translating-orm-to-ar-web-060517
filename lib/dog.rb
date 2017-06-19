4850377fca515764a5ed6cc1d7b4b1f695074233
github username <github email address>
2017-06-18 23:56:30 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,26 @@
-class Dog
+
+
+class Dog < ActiveRecord::Base
+  # attr_accessor :name, :breed
+  #
+  #
+  # # def self.create
+  # #   sql = <<-SQL
+  # #   CREATE TABLE IF NOT EXISTS songs (
+  # #   id INTEGER PRIMARY KEY,
+  # #   title TEXT,
+  # #   length INTEGER
+  # #   )
+  # #   SQL
+  # #   ActiveRecord::Base.DB.execute(sql)
+  # # end
+  #
+  # def initialize(hash_object = {})
+  #   @name = hash_object[:name]
+  #   @breed = hash_object[:breed]
+
+  # end
+
+
+
 end
